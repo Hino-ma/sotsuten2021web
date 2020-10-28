b76608152b2a3ff66477e23b026030fe44edf98f
hinobori <h.mai0129@gmail.com>
2020-10-29 04:28:22 +0900

    add new page

--- web/pages/research/index.vue
+++ web/pages/research/index.vue
@@ -1,20 +1,23 @@
 <template lang="pug">
   div.container
-    div.container_list.top
 
-    div.container_list.hamon
-      h2 波紋
+    
+    div.container_list.introduction
+      h2 研究・活動の紹介
       p
-        | 私たち公立はこだて未来大学情報デザインコースは、情報社会で豊かに生きるためのデザイン理論や<br>表現方法に加え、情報科学屋認知科学の最新の理論と実践を学んできました。<br><br>
-        | これまでの集大成として「波紋」をテーマとした社会に一石を投じる<br>新たなデザインの可能性を提示する展覧会を開催します。<br><br>
-        | 本展覧会が社会へ波紋を呼び、形を変えながら新たな波を起こし、<br>起こし続けるきっかけとなることを期待しています。
+        | 今年度の卒業展覧会は新型コロナウイルスの感染拡大防止の観点から、<br>対面形式ではなくオンラインでの開催といたします。<br><br>
+        | そのため、研究活動や作品紹介は当サイトにて常設展示いたします。<br>研究者本人からのコメントや動画での紹介も行っております。<br>是非ごゆっくりご覧ください。<br><br>
+    div.container_list.session
+      h2 オンラインセッション
+      p
+        | 出展者と来場者の皆様との対話の場は日時を設定の上、実施いたします。（詳細はこちらをご覧ください）<br><br>
+        | 例年と異なる形式となりますが、より多くの方々にご来場いただけることを楽しみにしております。
 
 </template>
 
 <script>
 import Card from '~/components/Card.vue'
 import Footer from '~/components/Footer.vue'
-import Title from '~/components/Title.vue'
 import Header from '~/components/Header.vue'
 import Title from '~/components/Title.vue'
 
